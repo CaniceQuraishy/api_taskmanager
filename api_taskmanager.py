25e64a689da151b2440a96cb630a581824ae5412
Canice Musa Quraishy <canice.quraishy@strathmore.edu>
2024-10-28 21:20:00 +0300

    Class definition and adding attributes

---
+++ api_taskmanager.py
@@ -0,0 +1,6 @@
+class Task:
+    def __init__(self, title, description, due_date):
+        self.title = title          # Title of the task
+        self.description = description  # Description of the task
+        self.due_date = due_date    # Due date for the task
+        self.is_completed = False    # Status of the task, set to not completed initially
